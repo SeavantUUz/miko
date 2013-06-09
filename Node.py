a4984afe747ba16a9a6bc66081388c45ea186a03
AprocySanae <axdiaoqi220@gmail.com>
2013-06-09 12:08:15 +0000

    remove Node class from shoujo.py

---
+++ Node.py
@@ -0,0 +1,44 @@
+# coding:urt-8
+## each article
+## you can also add some other properties
+class Node(object):
+    def __init__(self,*args,**kwargs):
+        try:
+            self._title = kwargs['title']
+            self._timestamp = kwargs['timestamp']
+            self._path = kwargs['path']
+            self._tags = kwargs['tags']
+            self._content = kwargs['content']
+            self._archive = kwargs['archive']
+            self._abstrct = kwargs['abstrct']
+
+        except KeyError:
+            raise Exception('Ouch! Properties not enough!!\n')
+    
+    @property
+    def Title(self):
+        return self._title
+
+    @property
+    def TimeStamp(self):
+        return self._timestamp
+
+    @property
+    def Path(self):
+        return self._path
+
+    @property
+    def Tags(self):
+        return self._tags
+
+    @property
+    def Content(self):
+        return self._content
+
+    @property
+    def Archive(self):
+        return self._archive
+
+    @property
+    def Abstrct(self):
+        return self._abstrct
