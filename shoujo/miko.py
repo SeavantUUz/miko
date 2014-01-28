e432f2e3862649103daacab0b2465bb3c3ea5bae
AprocySanae <axdiaoqi220@gmail.com>
2014-01-27 20:44:26 -0600

    implement postDir

--- shoujo/miko.py
+++ shoujo/miko.py
@@ -1,22 +1,39 @@
-import codecs
+import codecs,os
 from shoujo.utils import save_nodes,get_nodes,parser
 from shoujo.node import Node
 from shoujo.pagination import Pagination
 
-@save_nodes
-@get_nodes
-def post(nodes,filename):
-    # post a article,elements is a dict
-    # submiited post maybe already in nodes
+def pagination(nodes,page,per_page=7):
+    return  Pagination(nodes,page,per_page)
+
+def _node(filename):
     elements = parser(codecs.open(filename,'r','utf-8'))
     node = Node(**elements)
+    return node
+
+@save_nodes
+@get_nodes
+@sort_nodes
+def post(nodes,filename):
+    '''post a article,elements is a dict'''
+    node = _node(filename)
     nodes = filter(lambda o:o.title != node.title,nodes)
     nodes.append(node)
     #_generatorHtml(nodes)
-    return sorted(nodes)
+    return nodes
 
-def pagination(nodes,page,per_page=7):
-    return  Pagination(nodes,page,per_page)
+
+@save_nodes
+@sort_nodes
+def postDir(dirname):
+    '''post dirs,which would try all files in dirs
+       and post them'''
+    # dirs = filter(lambda d:os.path.isdir(d),args)
+    # for d in dirs:
+    for root,dirs,files in os.walk(dirname):
+        nodes = map(_node,[os.path.join(root,f) for f in files])
+    return nodes
+
 
 @get_nodes
 def show(nodes):
@@ -47,3 +64,5 @@ def remove(nodes,index):
                 return nodes
             else:
                 return None
+
+
--- shoujo/utils.py
+++ shoujo/utils.py
@@ -1,18 +1,24 @@
 #coding:utf-8
-__all__ = ['get_nodes','BleepRenderer']
+__all__ = ['sort_nodes','get_nodes','save_nodes','BleepRenderer']
 
 import pickle
 
-def get_nodes(func):
-    '''A decorator for getting nodes'''
-    try:
-        pick = open('data.pick','rb')
-    except IOError:
-        nodes = []
-    else:
-        nodes = pickle.load(pick)
-        pick.close()
+def sort_nodes(func):
     def wrapper(*args,**kwargs):
+        return sorted(func(*args,**kwargs))
+    return wrapper
+
+def get_nodes(func):
+    '''A decorator for getting nodes,
+    import a key word mask to stop decorator.'''
+    def wrapper(*args,**kwargs):
+        try:
+            pick = open('data.pick','rb')
+        except IOError:
+            nodes = []
+        else:
+            nodes = pickle.load(pick)
+            pick.close()
         return func(nodes,*args,**kwargs)
     return wrapper
 
@@ -20,7 +26,7 @@ def save_nodes(func):
     '''A decorator for saving nodes'''
     def wrapper(*args,**kwargs):
         nodes = func(*args,**kwargs)
-        if nodes
+        if nodes:
             pick = open('data.pick','wb')
             pick.dump(nodes)
             pick.close()
